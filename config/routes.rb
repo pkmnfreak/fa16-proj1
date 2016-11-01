2e986f37412e20dce96ee195c30aba8e0a3bbcce
pkmnfreak <evcchang@berkeley.edu>
2016-11-01 15:23:39 -0700

    init

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :trainers
   resources :trainers
+  patch 'capture' => 'pokemons#capture'
+  patch 'damage' => 'pokemons#damage'
+  get '/pokemon/new', to: 'pokemons#new'
+  post '/pokemons', to: 'pokemons#create'
 end
